a77c16eea36ed967f338ce68cf7a5c930b284324
auliabismar <auliabismar@yahoo.com>
2025-09-12 14:40:19 +0000

    feat: implement donor form functionality and enhance data handling

--- bmh_ziswaf/ziswaf/doctype/donor/donor.js
+++ bmh_ziswaf/ziswaf/doctype/donor/donor.js
@@ -1,8 +1,12 @@
 // Copyright (c) 2025, PT Karya Tata Bangsa and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Donor", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on('Donor', {
+	refresh(frm) {
+    if(!frm,is_new()){
+      frappe.contacts.render_address_and_contact(frm);
+    } else {
+      frappe.contacts.clear_address_and_contact(frm);
+    }
+	},
+});
--- bmh_ziswaf/ziswaf/doctype/donor/donor.py
+++ bmh_ziswaf/ziswaf/doctype/donor/donor.py
@@ -1,9 +1,13 @@
 # Copyright (c) 2025, PT Karya Tata Bangsa and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
-
+from frappe.contacts.address_and_contact import (load_address_and_contact, delete_contact_and_address)
 
 class Donor(Document):
-	pass
+	def onload(self):
+		load_address_and_contact(self)
+	
+	def on_trash(self):
+		delete_contact_and_address(self.doctype, self.name)
